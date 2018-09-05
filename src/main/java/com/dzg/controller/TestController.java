d9441c5f7b76af54c9fa60ae9210d06f43159861
dzg <1395224335@qq.com>
2018-09-05 17:13:00 +0800

    chore:测试类

--- src/main/java/com/dzg/controller/TestController.java
+++ src/main/java/com/dzg/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.dzg.controller;
 
+import com.dzg.common.JsonData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
-    public String hello() {
+    public JsonData hello() {
         log.info("hello");
-        return "hello,permission";
+        return JsonData.success("hello,permission");
         // return JsonData.success("hello, permission");
     }
 }
